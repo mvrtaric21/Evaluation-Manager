a6a70ebb030193311aaa1ad82fcf7bda59e6d0a4
mvrtaric21 <mvrtaric21@student.foi.hr>
2023-05-22 01:20:03 +0200

    Forma za evaluacije povezana s repozitorijem evaluacija

--- Evaluation Manager/Evaluation Manager/FrmEvaluation.cs
+++ Evaluation Manager/Evaluation Manager/FrmEvaluation.cs
@@ -16,6 +16,8 @@ namespace Evaluation_Manager
     {
         private Student student;
 
+        public Student SelectedStudent { get; private set; }
+
         public FrmEvaluation(Student selectedStudent)
         {
             InitializeComponent();
@@ -61,6 +63,20 @@ namespace Evaluation_Manager
            currentActivity.MaxPoints;
             numPoints.Minimum = 0;
             numPoints.Maximum = currentActivity.MaxPoints;
+            var evaluation = EvaluationRepository.GetEvaluation(SelectedStudent, currentActivity);
+            if (evaluation != null)
+            {
+                txtTeacher.Text = evaluation.Evaluator.ToString();
+                txtEvaluationDate.Text = evaluation.EvaluationDate.ToString();
+                numPoints.Value = evaluation.Points;
+            }
+            else
+            {
+                txtTeacher.Text = FrmLogin.LoggedTeacher.ToString();
+                txtEvaluationDate.Text = "-";
+                numPoints.Value = 0;
+            }
+
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
